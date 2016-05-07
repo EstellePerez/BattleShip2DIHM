0282b0c42f18cc62245dc30d8b84a173213188dd
Schlipp <eric.churlet@gmail.com>
2016-05-07 12:24:14 +0200

    Added files via upload

---
+++ Plop.java
@@ -0,0 +1,105 @@
+Composants:
+BoardModel *3 playboard, compboard, visuboard
+bool[5] *2 PlayShipSet, CompShipSet
+switch (ship type)
+4 boutons pour les 4 directions
+turn
+skillLevel compLVL=BEGINNER;
+bool[10][10] compshots=false;
+coor2D lastCompHit;
+
+initialisation:
+new BoardModel(Available) *2
+
+        ships placement:
+        select ship type (switch)
+        select start case ->cellModel startcell
+        si ship/= submarine
+                show available directions
+                        testDirection(cellModel startcell, direction dir, ShipType bateau) {
+                            bool ok=true;
+                            cellModel test=startcell;
+                            int i=0;
+                            while(i<bateau.size-1 && ok) {
+                                if (adjacentCell(test, dir).CellType==Available) {
+                                    i++
+                                    test=(adjacentCell(test, dir)
+                                }
+                                else ok=false;
+                            }
+                            return ok;
+                        }
+        select direction
+        placeShip(cellModel startcell, direction dir, ShipType bateau) {
+            cellModel cell=startcell;
+            for i=0 i<bateau.getsize {
+                cell.setCellType(shipTypeToCellType(bateau));
+                cell=(adjacentCell(cell, dir);
+            }
+        }
+        replaceAll(available,ocean)
+
+
+        pour Ordi: randomCell(available, true) pour startcell
+
+        pour visuboard :replaceALL(available, unknown)
+
+
+Jeu:
+    coord2D coord;
+if turn==players {
+        get cell clicked->cellModel cellclicked
+        coord=visuboard.cellCoord(cellclicked);
+        if compboard.getcellmodel(coord.row,coord.column).getCellType().isAShip() {
+            compfleet.updateHits(cellTypeToShipType(compboard.getcellmodel(coord.row,coord.column).getCellType()))
+            compboard.getcellmodel(coord.row,coord.column).setCellType(HIT);
+            visuboard.getcellmodel(coord.row,coord.column).setCellType(HIT);
+        }
+        else {
+            visuboard.getcellmodel(coord.row,coord.column).setCellType(OCEAN);
+        }
+        turn=COMPUTER;
+}
+
+//bool[10][10] compshots
+//coor2D lastCompHit
+
+else {// turn==computer
+    if compLVL==BEGINER {
+        bool test=false;
+        while !test {
+            coord=playboard.cellCoords(playboard.randomCell(AVAILABLE, false));
+            if !compshots[coord.row][coord.column] {
+                compshots[coord.row][coord.column]=true;
+                test=true;
+                if playboard.getcellmodel(coord.row,coord.column).getCellType().isAShip() {
+                    playfleet.updateHits(cellTypeToShipType(playboard.getcellmodel(coord.row,coord.column).getCellType()))
+                    playboard.getcellmodel(coord.row,coord.column).setCellType(HIT);
+                    lastCompHit=coord;
+                }
+            }
+        }
+    }
+    turn=PLAYER;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
